bae1c15dee7e09240edb82535972a53806b71a57
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-07-08 11:52:45 +0300

    banner-UI component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { ProjectPitchesComponent } from './component/project-pitches/project-pitches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ProjectPitchesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/project-pitches/project-pitches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-pitches',
+  templateUrl: './project-pitches.component.html',
+  styleUrls: ['./project-pitches.component.css']
+})
+export class ProjectPitchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
